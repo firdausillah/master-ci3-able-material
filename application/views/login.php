154a6c4bc042dc9f991f590d9af6b13bbb2252f6
firdausillah <firdausillah123@gmail.com>
2021-12-08 19:38:28 +0700

    login

--- application/views/login.php
+++ application/views/login.php
@@ -98,14 +98,15 @@
                 <div class="col-sm-12">
                     <!-- Authentication card start -->
                     <form class="md-float-material form-material" method="POST" action="<?= base_url('auth/login_admin') ?>">
-                        <div class="text-center">
-                            <img src="<?= base_url() ?>assets/images/logo.png" alt="logo.png">
+                        <div class="text-center my-2">
+                            <img src="<?= base_url() ?>assets/images/logo-pendar-transparan.png" height="60px" alt="logo.png">
+                            <!-- <h3 class="text-white">Pendar.std</h3> -->
                         </div>
                         <div class="auth-box card">
                             <div class="card-block">
                                 <div class="row m-b-20">
                                     <div class="col-md-12">
-                                        <h3 class="text-center">Sign In</h3>
+                                        <h3 class="text-center">Login</h3>
                                     </div>
                                 </div>
                                 <div class="form-group form-primary">
@@ -134,7 +135,7 @@
                                 </div>
                                 <div class="row m-t-30">
                                     <div class="col-md-12">
-                                        <button type="submit" class="btn btn-primary btn-md btn-block waves-effect waves-light text-center m-b-20">Sign in</button>
+                                        <button type="submit" class="btn btn-primary btn-md btn-block waves-effect waves-light text-center m-b-20">Login</button>
                                     </div>
                                 </div>
                                 <hr />
